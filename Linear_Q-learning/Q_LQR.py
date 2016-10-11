52619b75a2e24b31b63ed92c751f1116558c75ad
isaaczst <isaaczst@gmail.com>
2016-10-11 16:54:48 -0300

    first commit

---
+++ Linear_Q-learning/Q_LQR.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 07:21:41 2016
+
+@author: isaaczst
+"""
+
+from InputSetPoint import InputSetPoint
+import Plant
+import Task
+
+inputSetPoint=InputSetPoint(1)
+plant=Plant(1)
+task=Task(1,inputSetPoint,plant)
+def main():
+    for i in range(task.timeStepRange):
+        task.simu()
+        task.approxQ()
+        task.genAction()
+    task.plotResult()
+    
+
+if __name__ == '__main__':
+  main()
+
+
+
